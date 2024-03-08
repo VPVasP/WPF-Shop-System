05f1765b4a1b52c5dfb7ae6439b8f0c61947c211
Vp <vasilhspinis@gmail.com>
2024-03-08 12:44:39 +0200

    Item Classes and Constructors Added

---
+++ ShopApplicationWPF/ShopApplication/Item.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ShopApplication
+{
+   public class Item
+    {
+        public int Id { get; set; }
+        public required string itemName { get; set; }
+
+        public int price { get; set; }
+    }
+}
--- ShopApplicationWPF/ShopApplication/MainWindow.xaml.cs
+++ ShopApplicationWPF/ShopApplication/MainWindow.xaml.cs
@@ -1,18 +1,31 @@
 ﻿using System.Windows;
 namespace ShopApplication
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+        private int Currency;
+        private int ItemID;
+        private int itemPrice;
+        private bool isItemBought;
+        private List<Item> itemsList;
         public MainWindow()
         {
             InitializeComponent();
+            itemsList = new List<Item>();
         }
-        public void BuyItems(int itemID,int price)
+        public void BuyItems(Item item)
         {
-
+            itemsList.Add(item);
+            Currency -= item.price;
+        }
+        public void SellItems(Item item)
+        {
+            if (isItemBought)
+            {
+                itemsList.Remove(item);
+                Currency += item.price;
+            }
         }
     }
 }
