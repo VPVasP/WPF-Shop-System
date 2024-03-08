6421ce0c78ea1839ce1829c2e6f59e0b4a26b1bd
Vp <vasilhspinis@gmail.com>
2024-03-08 16:06:36 +0200

    Added The First Item And UI Button

--- ShopApplicationWPF/ShopApplication/MainWindow.xaml.cs
+++ ShopApplicationWPF/ShopApplication/MainWindow.xaml.cs
@@ -1,25 +1,58 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Net.Http;
+using System.Windows;
+using System.Windows.Controls;
 namespace ShopApplication
 {
 
     public partial class MainWindow : Window
     {
         private int Currency;
-        private int ItemID;
-        private int itemPrice;
         private bool isItemBought;
         private List<Item> itemsList;
         public MainWindow()
         {
             InitializeComponent();
             itemsList = new List<Item>();
+            FindButtons();
+            InitializeValues();
         }
-        public void BuyItems(Item item)
+        private void InitializeValues()
+        {
+            Currency = 20;
+        }
+        private void FindButtons()
+        {
+            BuyMilkButton = (Button)FindName("BuyMilkButton");
+        }
+
+        private void BuyMilk(object sender, RoutedEventArgs e)
+        {
+            Item item = new Item
+            {
+                itemName = "Milk",
+                price = 5,
+                Id = 0
+            };
+            Debug.WriteLine(Currency);
+            if (Currency >= item.price)
+            {
+                itemsList.Add(item);
+                Currency -= item.price;
+                Debug.WriteLine("Bought " + item.itemName);
+            }
+            else
+            {
+                Debug.WriteLine("We don't have Enough Money");
+            }
+   
+        }
+        public void BuyItems(Item item, object sender, RoutedEventArgs e)
         {
             itemsList.Add(item);
             Currency -= item.price;
         }
-        public void SellItems(Item item)
+        public void SellItems(Item item, object sender, RoutedEventArgs e)
         {
             if (isItemBought)
             {
