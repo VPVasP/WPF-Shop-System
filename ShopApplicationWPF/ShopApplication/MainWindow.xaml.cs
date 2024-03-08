9569f66eb83daee7569a6f6b5683a59fc5978d22
Vp <vasilhspinis@gmail.com>
2024-03-08 12:30:33 +0200

    Initial Commit

---
+++ ShopApplicationWPF/ShopApplication/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+namespace ShopApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void BuyItems(int itemID,int price)
+        {
+
+        }
+    }
+}
