e5d1e19fb9906b477ddab85f0349f6c7d476d210
Vp <vasilhspinis@gmail.com>
2024-03-11 14:48:40 +0200

    Added UI Texts and Improvements in the Code
    -Added UI Texts that show the quantity of the products we bought.
    -Code Improvements

--- ShopApplicationWPF/ShopApplication/MainWindow.xaml.cs
+++ ShopApplicationWPF/ShopApplication/MainWindow.xaml.cs
@@ -32,33 +32,37 @@ namespace ShopApplication
             SellBreadButton.Visibility = Visibility.Collapsed;
             BreadInventoryQuantity.Visibility = Visibility.Collapsed;
             SellEggsButton.Visibility = Visibility.Collapsed;
+            EggsInventoryQuantity.Visibility = Visibility.Collapsed;
             SellCheeseButton.Visibility = Visibility.Collapsed;
+            CheeseInventoryQuantity.Visibility = Visibility.Collapsed;
             SellTomatoButton.Visibility = Visibility.Collapsed;
+            TomatoInventoryQuantity.Visibility = Visibility.Collapsed;
             SellAppleButton.Visibility = Visibility.Collapsed;
+            AppleInventoryQuantity.Visibility = Visibility.Collapsed;
         }
         private void InitializeItems()
         {
-            itemsList.Add(new Item("Milk", 3, 0,0));
+            itemsList.Add(new Item("Milk", 3, 0, 0));
             MilkProductName.Text = "Milk";
-            MilkPrice.Text = "Price: 3";
-            itemsList.Add(new Item("Bread",5, 1,0));
-            BreadProductName.Text ="Bread";
-            BreadPrice.Text = "Price: 5";
-            itemsList.Add(new Item("Eggs", 7, 2,0));
+            MilkPrice.Text = "Price: 3$";
+            itemsList.Add(new Item("Bread", 5, 1, 0));
+            BreadProductName.Text = "Bread";
+            BreadPrice.Text = "Price: 5$";
+            itemsList.Add(new Item("Eggs", 7, 2, 0));
             EggProductName.Text = "Egg";
-            EggPrice.Text = "Price: 7";
-            itemsList.Add(new Item("Cheese",10, 3,0));
+            EggPrice.Text = "Price: 7$";
+            itemsList.Add(new Item("Cheese", 10, 3, 0));
             CheeseProductName.Text = "Cheese";
-            CheesePrice.Text = "Price: 10";
-            itemsList.Add(new Item("Tomato", 15,4,0));
+            CheesePrice.Text = "Price: 10$";
+            itemsList.Add(new Item("Tomato", 15, 4, 0));
             TomatoProductName.Text = "Tomato";
-            TomatoPrice.Text = "Price: 15";
-            itemsList.Add(new Item("Tomato", 15,5,0));
+            TomatoPrice.Text = "Price: 15$";
+            itemsList.Add(new Item("Tomato", 15, 5, 0));
             TomatoProductName.Text = "Tomato";
-            TomatoPrice.Text = "Price: 15";
-            itemsList.Add(new Item("Apple", 20,6,0));
+            TomatoPrice.Text = "Price: 15$";
+            itemsList.Add(new Item("Apple", 20, 6, 0));
             AppleProductName.Text = "Apple";
-            ApplePrice.Text = "Price: 20";
+            ApplePrice.Text = "Price: 20$";
 
 
         }
@@ -68,6 +72,7 @@ namespace ShopApplication
 
             SellMilkButton.Visibility = Visibility.Visible;
             MilkInventoryQuantity.Visibility = Visibility.Visible;
+            MilkInventoryQuantityUI();
         }
 
         private void BuyBread(object sender, RoutedEventArgs e)
@@ -75,102 +80,167 @@ namespace ShopApplication
             BuyItems("Bread");
             SellBreadButton.Visibility = Visibility.Visible;
             BreadInventoryQuantity.Visibility = Visibility.Visible;
+            BreadInventoryQuantityUI();
         }
 
         private void BuyEggs(object sender, RoutedEventArgs e)
         {
             BuyItems("Eggs");
             SellEggsButton.Visibility = Visibility.Visible;
+            EggsInventoryQuantity.Visibility = Visibility.Visible;
+            EggsInventoryQuantityUI();
         }
         private void BuyCheese(object sender, RoutedEventArgs e)
         {
             BuyItems("Cheese");
             SellCheeseButton.Visibility = Visibility.Visible;
+            CheeseInventoryQuantity.Visibility = Visibility.Visible;
+            CheeseInventoryQuantityUI();
         }
         private void BuyTomato(object sender, RoutedEventArgs e)
         {
             BuyItems("Tomato");
             SellTomatoButton.Visibility = Visibility.Visible;
+            TomatoInventoryQuantity.Visibility = Visibility.Visible;
+            TomatoInventoryQuantityUI();
         }
         private void BuyApple(object sender, RoutedEventArgs e)
         {
             BuyItems("Apple");
             SellAppleButton.Visibility = Visibility.Visible;
+            AppleInventoryQuantity.Visibility = Visibility.Visible;
+            AppleInventoryQuantityUI();
         }
 
-        private void UpdateCurrencyText()
-        {
-            CurrencyText.Text ="Currency: "+ Currency.ToString();
-        }
-
-
+        
         private void SellMilk(object sender, RoutedEventArgs e)
         {
-           SellItems("Milk");
-
+            SellItems("Milk");
+            MilkInventoryQuantityUI();
         }
         private void SellBread(object sender, RoutedEventArgs e)
         {
             SellItems("Bread");
-
+            BreadInventoryQuantityUI();
         }
 
         private void SellEggs(object sender, RoutedEventArgs e)
         {
             SellItems("Eggs");
-
+            EggsInventoryQuantityUI();
         }
 
         private void SellCheese(object sender, RoutedEventArgs e)
         {
             SellItems("Cheese");
-
+            CheeseInventoryQuantityUI();
         }
 
         private void SellTomato(object sender, RoutedEventArgs e)
         {
             SellItems("Tomato");
-
+            TomatoInventoryQuantityUI();
         }
         private void SellApple(object sender, RoutedEventArgs e)
         {
             SellItems("Apple");
-
+            AppleInventoryQuantityUI();
         }
 
 
         public void BuyItems(string itemName)
         {
-            
-                Item item = itemsList.Find(item => item.itemName == itemName);
-                if (item != null)
+
+            Item item = itemsList.Find(item => item.itemName == itemName);
+            if (item != null)
+            {
+                if (Currency >= item.price)
                 {
-                    if (Currency >= item.price)
-                    {
-                        Currency -= item.price;
-                        Debug.WriteLine("Bought " + item.itemName);
-                        UpdateCurrencyText();
-                        BoughtItemText.Text ="You Bought "+ item.itemName;
-                        boughtItemsList.Add(item);
-                        item.quantity += 1;
-                       
-                    }
+                    Currency -= item.price;
+                    Debug.WriteLine("Bought " + item.itemName);
+                    UpdateCurrencyText();
+                    BoughtItemText.Text = "You Bought " + item.itemName + " for " + item.price + "$";
+                    boughtItemsList.Add(item);
+                    item.quantity += 1;
+
                 }
-            
+            }
+
         }
         public void SellItems(string itemName)
         {
             Item item = itemsList.Find(item => item.itemName == itemName);
-            if (item != null &&item.quantity>=1)
+            if (item != null && item.quantity >= 1)
             {
                 Currency += item.price;
                 UpdateCurrencyText();
                 boughtItemsList.Remove(item);
                 Debug.WriteLine("Sold " + item.itemName);
-                BoughtItemText.Text = "You Sold " + item.itemName;
+                BoughtItemText.Text = "You Sold " + item.itemName + " for " + item.price +"$"; 
                 item.quantity -= 1;
-           
+
             }
         }
+        private void MilkInventoryQuantityUI()
+        {
+            Item item = itemsList.Find(item => item.itemName == "Milk");
+            if (item != null)
+            {
+                MilkInventoryQuantity.Text = item.itemName + " Quantity: " + item.quantity.ToString();
+            }
+        }
+        private void BreadInventoryQuantityUI()
+        {
+            Item item = itemsList.Find(item => item.itemName == "Bread");
+            if (item != null)
+            {
+                BreadInventoryQuantity.Text = item.itemName + " Quantity: " + item.quantity.ToString();
+            }
+        }
+
+        private void EggsInventoryQuantityUI()
+        {
+            Item item = itemsList.Find(item => item.itemName == "Eggs");
+            if (item != null)
+            {
+                EggsInventoryQuantity.Text = item.itemName + " Quantity: " + item.quantity.ToString();
+            }
+        }
+
+        private void CheeseInventoryQuantityUI()
+        {
+            Item item = itemsList.Find(item => item.itemName == "Cheese");
+            if (item != null)
+            {
+                CheeseInventoryQuantity.Text = item.itemName + " Quantity: " + item.quantity.ToString();
+            }
+        }
+
+        private void TomatoInventoryQuantityUI()
+        {
+            Item item = itemsList.Find(item => item.itemName == "Tomato");
+            if (item != null)
+            {
+                TomatoInventoryQuantity.Text = item.itemName + " Quantity: " + item.quantity.ToString();
+            }
+        }
+
+        private void AppleInventoryQuantityUI()
+        {
+            Item item = itemsList.Find(item => item.itemName == "Apple");
+            if (item != null)
+            {
+                AppleInventoryQuantity.Text =item.itemName+ " Quantity: " + item.quantity.ToString();
+            }
+        }
+        private void UpdateCurrencyText()
+        {
+            CurrencyText.Text = "Currency: " + Currency.ToString() + "$";
+        }
+
+
     }
+
+
+
 }
